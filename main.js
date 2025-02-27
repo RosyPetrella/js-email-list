642ed6936c3b3b4fdfacacbcfdaf7e7ababf8490
RosyPetrella <petrellamr@gmail.com>
2025-02-27 14:32:04 +0100

    add fetch and for loop

---
+++ main.js
@@ -0,0 +1,29 @@
+// Descrizione;
+// Attraverso l'apposita API di Boolean
+// https://flynn.boolean.careers/exercises/api/random/mail
+// generare 10 indirizzi email e stamparli in pagina all'interno di una lista.
+// Bonus
+// Abbellire con CSS o Bootstrap
+// Inserire un bottone che al click faccia il fetch altre 10 mail (sostituendo le altre)
+
+const divEl = document.querySelector(".display");
+const listEl = document.querySelector(".list");
+console.log(divEl, listEl);
+
+function generateEmail() {
+  listEl.innerHTML = "";
+
+  for (let i = 0; i < 10; i++) {
+    fetch("https://flynn.boolean.careers/exercises/api/random/mail")
+      .then((response) => response.json())
+      .then((data) => {
+        const email = data.response;
+        console.log(email);
+
+        listEl.insertAdjacentHTML("beforeend", `<li>${email}</li>`);
+      })
+      .catch((error) => console.error("error", error));
+  }
+}
+
+generateEmail();
